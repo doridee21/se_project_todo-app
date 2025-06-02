9914e163deaff1d8d3272f12917b829d2ef2987b
doridee21 <dorion.miller90@gmail.com>
2025-06-02 11:25:56 -0400

    updates to project

--- components/Popup.js
+++ components/Popup.js
@@ -4,20 +4,32 @@ class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
+    this._handleEscapeClose = this._handleEscapeClose.bind(this);
+    this._handleOverlayClick = this._handleOverlayClick.bind(this);
   }
 
   _handleEscapeClose(evt) {
     if (evt.key === "Escape") {
-      console.log("escape was pressed");
+      //console.log("escape was pressed");
       // find the currently opened modal
       // and close it
       this.close();
     }
   }
 
+  _handleOverlayClick(evt) {
+    console.log("Overlay was pressed");
+    // Check if the click target is the popup element itself (the overlay)
+    if (evt.target === this._popupElement) {
+      this.close();
+    }
+  }
+
   open() {
     this._popupElement.classList.add("popup_visible");
-    document.addEventListener("keydown", this._handleEscapeClose);
+    document.addEventListener("keydown", (event) => {
+      this._handleEscapeClose(event);
+    });
   }
 
   close() {
@@ -32,9 +44,19 @@ class Popup {
     this._popupCloseBtn.addEventListener("click", () => {
       this.close();
     });
-    this._popupElement.addEventListener("mousedown", (evt) => {
-      this.close();
-    });
+
+    this._popupElement.addEventListener("mousedown", this._handleOverlayClick);
+
+    // combined listeners
+
+    //this._popupElement.addEventListener("mousedown", (evt) => {
+    //  if (
+    //    this._popupElement /*"popup__close"*/ ||
+    //    this._popupCloseBtn /*"popup"*/
+    //  ) {
+    //    this.close();
+    //  }
+    //});
   }
 }
 
